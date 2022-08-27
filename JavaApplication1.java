7f74c5532f41e77e528b17483dd44c8d8f2d7d30
Gustavo Gomes Ferreira <102599068+Gus027@users.noreply.github.com>
2022-08-26 21:35:30 -0300

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -15,33 +15,29 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner tcl = new Scanner(System.in);
-        //VARIAVEIS
+        //variaveis
         int ns = 10;
-        int cht=0;
-        int tntvs=0;
+        int cht = 0;
+        int tntvs = 0;
         //OBETER
-        while(tntvs<16){
-        System.out.println("Insira um numero: ");
-        cht = tcl.nextInt();
-        tntvs +=1;
-        if(cht == ns){
-            System.out.println("Parabens voce acertou!");
-            break;
+        while (tntvs < 16) {
+            System.out.println("Insira um numero: ");
+            cht = tcl.nextInt();
+            tntvs += 1;
+            if (cht == ns) {
+                System.out.println("Parabens voce acertou!");
+                break;
+            } else if (tntvs >= 3 & tntvs <=5) {
+                System.out.println("DICA 01: Digite um numero de 1 a 1000");
+            } else if (tntvs >= 8 & tntvs < 10) {
+                System.out.println("DICA 02: Digite um numero de 1 a 100");
+            } else if (tntvs >= 13 & tntvs <= 15) {
+                System.out.printf("DICA 3: Digite um numero de 1 a 10");
+            } else if(tntvs == 16){
+                System.out.println("FIM!");
+            }
         }
-        else if(tntvs <= 3 || tntvs <= 5){
-              System.out.println("DICA 1");
-        }
-        else if(tntvs <=8 || tntvs <=10){
-                System.out.println("DICA 2");
-       }
-        else if (tntvs == 13){
-            System.out.println("DICA 3");
-        }
-        else if (tntvs == 16){
-             System.out.println("FIM");
-      }
-    }    
-  }  
+    }
 }
